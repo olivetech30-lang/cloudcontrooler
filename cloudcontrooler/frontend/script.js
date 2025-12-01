840bae070526431370f1ec396e2533e33719cf8a
olivetech30-lang <olivetech30@gmail.com>
2025-12-01 10:52:37 +0500

    Update script.js

--- cloudcontrooler/frontend/script.js
+++ cloudcontrooler/frontend/script.js
@@ -5,7 +5,7 @@ const MAX_DELAY = 2000;
 const BUTTON_STEP = 50;
 
 // Backend API base URL
-const BACKEND_URL = "https://cloudcontrooler.vercel.app"; // e.g. https://flash-backend-yourname.vercel.app
+const BACKEND_URL = "https://cloudcontrooler-backend.vercel.app"; // e.g. https://flash-backend-yourname.vercel.app
 const DELAY_API_URL = `${BACKEND_URL}/api/delay`;
 
 let currentDelay = 700;
@@ -105,4 +105,5 @@ window.addEventListener("load", () => {
   startPolling();
 
 });
+
 
